d279b333ee2a877e4bc636e0f817d429e0ea1d62
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-11-26 10:34:17 +0100

    Solucionado error db

---
+++ index.php
@@ -0,0 +1,17 @@
+<<<<<<< Updated upstream
+=======
+<h1> RESTAURANTE PHP</h1>
+
+<?php
+
+//conexión la base de datos
+// require './db/db_conect.php';
+// $mysqli = conectar();
+
+// Añadimos el botón hacia la pantalla de nuevo plato
+echo '<a href="http://localhost/dev/EquipoRest/pages/listaplatos.php">
+<button>Ver Carta</button>
+</a>';
+
+?>
+>>>>>>> Stashed changes
