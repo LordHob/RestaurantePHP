4276efa680229b0c4f242be2af182dbabe46df61
LordHob <rafaelginergomez@gmail.com>
2021-11-26 13:09:42 +0100

    Nuevo header

--- pages/login.php
+++ pages/login.php
@@ -1,17 +1,23 @@
+<h1> LOGIN </h1>
+
+
 <?php
 //Consulta y conexión a BBDD
 require '../db/db_connect.php';
 $mysqli = conectar();
 
-$resultado = $mysqli->query("SELECT * FROM user WHERE id=" .$_GET["id"]);
-$reg=$resultado->fetch_assoc();
+$resultado = $mysqli->query("SELECT * FROM user WHERE id=" . $_GET["id"]);
+$reg = $resultado->fetch_assoc();
 
-function email_validation($str){  return (false !== strpos($str, "@") && false !== strpos($str, "."));}
+function email_validation($str)
+{
+    return (false !== strpos($str, "@") && false !== strpos($str, "."));
+}
 echo 'Pantalla de Login';
 echo '<form method="post" action="" name="signin-form">
     <div class="form-element">
         <label>email</label>
-        <input type="email" name='($str)."email".' required />   
+        <input type="email" name='($str) . "email" . ' required />   
     </div>
     <div class="form-element">
         <label>Password</label>
@@ -20,4 +26,3 @@ echo '<form method="post" action="" name="signin-form">
     <button type="submit" name="login" value="login">Log In</button>
 </form>';
 ?>
-
