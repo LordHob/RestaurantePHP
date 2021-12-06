9b5526f1a93442d749a4b5048196a836dd0c4b7e
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-12-06 17:01:28 +0100

    Crear registro funcional y CRUD de platos

---
+++ pages/Plato/action_putplato.php
@@ -0,0 +1,9 @@
+<?php
+//CONECTAR CON BASE DE DATOS
+require '../../db/db_connect.php';
+$mysqli = conectar();
+$resultado = $mysqli->query("UPDATE plato set titulo='$_POST[newtitulo]' WHERE titulo='$_POST[titulo]'");
+$resultadodos=$mysqli->query("UPDATE plato set comensales='$_POST[newcomensales]' WHERE comensales='$_POST[comensales]'");
+$resultadotres=$mysqli->query("UPDATE plato set tipo='$_POST[newtipo]' WHERE tipo='$_POST[tipo]'");
+header('Location: http://localhost/dev/EquipoRest/pages/Plato/listaplatos.php');
+?>
--- pages/Plato/listaplatos.php
+++ pages/Plato/listaplatos.php
@@ -3,7 +3,7 @@
 <?php
 
 //conexión la base de datos
-require '../db/db_connect.php';
+require '../../db/db_connect.php';
 $mysqli = conectar();
 
 //Consulta BBDD
@@ -13,14 +13,24 @@ $resultado = $mysqli->query("SELECT id, titulo FROM plato");
 echo '<ol>';
 while ($reg = $resultado->fetch_assoc()) {
     echo '<li>';
-    echo '<a href="http://localhost/dev/EquipoRest/pages/detalle_plato.php?id=' . $reg['id'] . '&titulo=' . $reg['titulo'] . '">' . $reg['titulo'] . '</a>';
+    echo '<a href="http://localhost/dev/EquipoRest/pages/Plato/detalle_plato.php?id=' . $reg['id'] . '&titulo=' . $reg['titulo'] . '">' . $reg['titulo'] . '</a>';
     echo '</li>';
 }
 echo '</ol>';
 // Añadimos el botón hacia la pantalla de nuevo plato
-echo '<a href="http://localhost/dev/EquipoRest/pages/form_plato.html">
-<button>Añadir plato</button>
+echo '<a href="http://localhost/dev/EquipoRest/pages/Plato/añadir_plato.php">
+<button>Añadir un plato</button>
+</a>';
+
+// Añadimos el botón hacia la pantalla de actualizar plato
+echo '<a href="http://localhost/dev/EquipoRest/pages/Plato/actualizar_plato.php">
+<button>Actualizar un plato</button>
 </a>';
 
 
+// Añado botón para ir a borrar un plato
+
+echo '<a href="http://localhost/dev/EquipoRest/pages/Plato/delete_plato.php">
+<button type="button">Borrar un plato</button>
+</a>';
 ?>
