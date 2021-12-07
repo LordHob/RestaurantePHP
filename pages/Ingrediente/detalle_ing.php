32c69b22fc09a38da297174f3cac9b6ea98f3b69
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-12-07 17:32:07 +0100

    Añadir actualizar ingrediente

--- pages/Ingrediente/detalle_ing.php
+++ pages/Ingrediente/detalle_ing.php
@@ -16,6 +16,22 @@ echo '<li>Nombre del ingrediente: '.$reg['nombre'].'</li>';
 echo '<li>Cantidad '.$reg['cantidad'].'</li>';
 echo '</ul>';
 
+
+?>
+
+<div classname="formulario">
+    <form novalidate action="action_putingrediente.php" method="post">
+        <p>Nombre del ingrediente a actualizar: <input type="nombre" id="nombre" name="nombre" /></p>
+        <p>Nuevo nombre del ingrediente: <input type="newnombre" id="newnombre" name="newnombre" /></p>
+        <p>Número de cantidad a actualizar: <input type="cantidad" id="cantidad" name="cantidad" /></p>
+        <p>Nuevo número de cantidad: <input type="newcantidad" id="newcantidad" name="newcantidad" /></p>            </select></p>
+        <p><input type="submit" value="Actualizar plato" class="button" /></p>
+    </form>
+</div>
+
+<?php
+
+
 // Añado botón para volver a index
 
 echo '<a href="http://localhost/dev/EquipoRest/pages/Plato/listaplatos.php">
