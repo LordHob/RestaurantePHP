4e168de0ef881fe6626e237d0ef272f47de7f887
Rafael Giner <90702425+LordHob@users.noreply.github.com>
2021-12-08 19:24:48 +0100

    cambio en la conexión

--- db/db_connect.php
+++ db/db_connect.php
@@ -2,7 +2,7 @@
 
 function conectar(){
 //Conexión con base de datos
-$mysqli = new mysqli("eu-cdbr-west-01.cleardb.com:6969", "b59a9339d76260", "d37d3007", "heroku_26e728fc223c841");
+$mysqli = new mysqli("eu-cdbr-west-01.cleardb.com:3306", "b59a9339d76260", "d37d3007", "heroku_26e728fc223c841");
 //Control de errores
 if ($mysqli->connect_errno) {
     echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") "
