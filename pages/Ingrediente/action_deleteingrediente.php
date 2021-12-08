2de0e5d1f59af96bdc23421b32999908b8c6d307
Rafael Giner <90702425+LordHob@users.noreply.github.com>
2021-12-08 17:48:51 +0100

    Dirección del endpoint

--- pages/Ingrediente/action_deleteingrediente.php
+++ pages/Ingrediente/action_deleteingrediente.php
@@ -3,4 +3,4 @@
 require '../../db/db_connect.php';
 $mysqli = conectar();
 $resultado = $mysqli->query("DELETE FROM ingrediente WHERE nombre= '$_POST[nombre]'");
-header('Location: http://localhost/dev/EquipoRest/pages/Ingrediente/listaingredientes.php');
+header('Location: https://restaurante-php-equipo.herokuapp.com/pages/Ingrediente/listaingredientes.php');
